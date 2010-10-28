5ddd84a4a4d68c80c8650e6d20dac198d1d0d201
ddenev <ddenev@infno5800.ag5.mpi-sb.mpg.de>
2010-10-28 17:00:15 +0200

    removed not used job folders

--- temporal-coherence/Heritrix3/liwa/src/main/java/org/liwa/coherence/schedule/selective/TestCases.java
+++ temporal-coherence/Heritrix3/liwa/src/main/java/org/liwa/coherence/schedule/selective/TestCases.java
@@ -4,13 +4,19 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.liwa.coherence.schedule.SchedulablePage;
 import org.liwa.coherence.schedule.Selective;
 
 public class TestCases {
-	private static void testExhaustive(){
+	private enum Win {
+		L, W, S, X, P, E
+	};
+
+	private static void testExhaustive() {
 		int size = 10000;
 		List<Integer> schedule = new ArrayList<Integer>();
 		for (int i = 0; i < size; i++) {
@@ -18,85 +24,113 @@ public class TestCases {
 		}
 		List<List<Double>> changes = DataGenerator.getChangeRates(size);
 		List<List<Double>> weights = DataGenerator.getWeights(size);
-		
+
 		List<String> changeLabels = DataGenerator.getChangeRateLabels();
 		List<String> weightLabels = DataGenerator.getWeightLabels();
-		
-		for(int i = 0; i < changes.size(); i++){
-			testChanges(changes.get(i), weights, schedule,
-					changeLabels.get(i), weightLabels);
+
+		for (int i = 0; i < changes.size(); i++) {
+			testChanges(changes.get(i), weights, schedule, changeLabels.get(i),
+					weightLabels);
 		}
-		
+
 	}
-	
-	private static void testChanges(List<Double> changes, List<List<Double>>
-		weights, List<Integer> schedule, 
-		String changeLabel, List<String> weightLabels){
-		for(int i = 0; i < weights.size(); i++){
+
+	private static void testChanges(List<Double> changes,
+			List<List<Double>> weights, List<Integer> schedule,
+			String changeLabel, List<String> weightLabels) {
+		Map<Win, Integer> wins = new HashMap<Win, Integer>();
+		for (int i = 0; i < weights.size(); i++) {
 			List<Double> c = new ArrayList<Double>();
-			for(int j = 0; j < changes.size(); j++){
+			for (int j = 0; j < changes.size(); j++) {
 				c.add(changes.get(i));
 			}
 			System.out.println("-----------------------------------");
 			System.out.println(changeLabel + " : " + weightLabels.get(i));
-			testOrder(changes, weights.get(i));
+			Win w = testOrder(changes, weights.get(i));
+			if (wins.get(w) == null) {
+				wins.put(w, 1);
+			} else {
+				wins.put(w, wins.get(w) + 1);
+			}
 		}
+		System.out.println(wins);
 	}
 
-	private static void testOrder(List<Double> changes, List<Double> weights) {
+	private static Win testOrder(List<Double> changes, List<Double> weights) {
 		DecimalFormat dc = new DecimalFormat("##.##########");
-//
-//		System.out.println("Selective-exhaustive: ");
-//
-//		System.out.println("l    : "
-//				+ dc.format(testOrderCubic(changes, weights, 
-//						new ChangeRateComparator())));
-//		System.out.println("w    : "
-//				+ dc.format(testOrderCubic(changes, weights, 
-//						new WeightComparator())));
-//		System.out.println("w+l  : "
-//				+ dc.format(testOrderCubic(changes, weights, 
-//						new WeightChangeRateSumComparator())));
-//		System.out.println("w*l  : "
-//				+ dc.format(testOrderCubic(changes, weights, 
-//						new WeightChangeRateProductComparator())));
-//		System.out.println("w*e^l: "
-//				+ dc.format(testOrderCubic(changes, weights, 
-//						new WeightChangeRateExponentComparator())));
 
-		System.out.println("Selective-fast: ");
+		// System.out.println("Selective-exhaustive: ");
+		//
+		// System.out.println("l : "
+		// + dc.format(testOrderCubic(changes, weights,
+		// new ChangeRateComparator())));
+		// System.out.println("w : "
+		// + dc.format(testOrderCubic(changes, weights,
+		// new WeightComparator())));
+		// System.out.println("w+l : "
+		// + dc.format(testOrderCubic(changes, weights,
+		// new WeightChangeRateSumComparator())));
+		// System.out.println("w*l : "
+		// + dc.format(testOrderCubic(changes, weights,
+		// new WeightChangeRateProductComparator())));
+		// System.out.println("w*e^l: "
+		// + dc.format(testOrderCubic(changes, weights,
+		// new WeightChangeRateExponentComparator())));
 
-		System.out.println("l    : "
-				+ dc.format(testOrderSelective(changes, weights, 
-						new ChangeRateComparator())));
-		System.out.println("w    : "
-				+ dc.format(testOrderSelective(changes, weights, 
-						new WeightComparator())));
-		System.out.println("w+l  : "
-				+ dc.format(testOrderSelective(changes, weights, 
-						new WeightChangeRateSumComparator())));
-		System.out.println("w*l  : "
-				+ dc.format(testOrderSelective(changes, weights, 
-						new WeightChangeRateProductComparator())));
-		System.out.println("w*e^l: "
-				+ dc.format(testOrderSelective(changes, weights, 
-						new WeightChangeRateExponentComparator())));
+		//System.out.println("Selective-fast: ");
+		Win win = Win.L;
+
+		double l = testOrderSelective(changes, weights,
+				new ChangeRateComparator());
+		double m = l;
+//		System.out.println("l    : " + dc.format(l));
+		double w = testOrderSelective(changes, weights, new WeightComparator());
+		if (m < w) {
+			win = Win.W;
+			m = w;
+		}
+		System.out.print(dc.format(l) + " " + dc.format(w));
+//		double wsl = testOrderSelective(changes, weights,
+//				new WeightChangeRateSumComparator());
+//		if (m < wsl) {
+//			win = Win.S;
+//			m = wsl;
+//		}
+//		System.out.println("w+l  : " + dc.format(wsl));
+//		double wxl = testOrderSelective(changes, weights,
+//				new WeightChangeRateProductComparator());
+//		if (m < wxl) {
+//			win = Win.X;
+//			m = wxl;
+//		}
+//		System.out.println("w*l  : " + dc.format(wxl));
+//		double wxel = testOrderSelective(changes, weights,
+//				new WeightChangeRateExponentComparator());
+//		if (m < wxl) {
+//			win = Win.P;
+//			m = wxel;
+//		}
+//		System.out.println("w*e^l: " + dc.format(wxel));
+		if(l == w ){
+			win = Win.E;
+		}
+		return win;
 	}
 
 	private static double testOrderCubic(List<Double> changes,
-			List<Double> weights,
-			Comparator<WeightedChangeRate> c) {
+			List<Double> weights, Comparator<WeightedChangeRate> c) {
 		resort(changes, weights, c);
 		Selective s = new Selective(changes, weights);
 		return s.exhaustive();
 	}
 
 	private static double testOrderSelective(List<Double> changes,
-			List<Double> weights,
-			Comparator<WeightedChangeRate> c) {
+			List<Double> weights, Comparator<WeightedChangeRate> c) {
 		resort(changes, weights, c);
+//		System.out.println(changes.get(0)+"/"+weights.get(0)+
+//				" " + changes.get(1)+"/"+weights.get(1));
 		Selective s = new Selective(changes, weights);
-//		System.out.println(s.getChangeRates());
+		// System.out.println(s.getChangeRates());
 		double d = s.selective(new ArrayList<SchedulablePage>(),
 				new ArrayList<SchedulablePage>(), 1.0);
 		// double t = 0;
@@ -106,29 +140,58 @@ public class TestCases {
 		// t += (weights.get(i)*Math.exp(-changes.get(i)*2*i*0.4));
 		// }
 		// System.out.println(t);
-//		System.out.println(s.getChangeRates());
-		System.out.println(s.getChangeRates());
+		// System.out.println(s.getChangeRates());
 		return d;
 	}
-	
-	
-	private static void testExcel(){
-		int size = 4;
-		//List<Double> weights = DataGenerator.generateLinear(size);
+
+	private static void testExcel() {
+		int size = 50;
+		// List<Double> weights = DataGenerator.generateLinear(size);
 		List<Double> changeRates = DataGenerator.generateLinear(size);
-		List<Double> weights = getInterestingWeights();
-//		testOrder(changeRates, weights);
-//		Collections.reverse(weights);
-//		testOrder(changeRates, weights);
-//		testOrder(changeRates, getInterestingWeights());
-//		testOrder(changeRates, getMoreInterestingWeights());
-		for(int i = 0; i < 15; i++){
+		List<Double> weights = DataGenerator.generateLinear(size);
+		testOrder(changeRates, weights);
+		// Collections.reverse(weights);
+		// testOrder(changeRates, weights);
+		// testOrder(changeRates, getInterestingWeights());
+		// testOrder(changeRates, getMoreInterestingWeights());
+		for (int i = 0; i < 30; i++) {
 			Collections.shuffle(weights);
-			System.out.println(weights);
 			testOrder(changeRates, weights);
 		}
- 	}
-	
+	}
+
+	private static void testSize() {
+		int size = 1000;
+		double end = 0.9;
+		DecimalFormat dc = new DecimalFormat("##.######");
+		Map<Win, Integer> wins = new HashMap<Win, Integer>();
+		System.out.print("\n      L           w");
+		for (double chStart = 0.001; chStart < 0.9; chStart += 0.003) {
+			List<Double> changeRates = DataGenerator.generateLinear(chStart,
+					end, size);
+			
+			double chStep = (end-chStart)/(size-1);
+			System.out.print("\n"+dc.format(chStart)+ " ");
+			for (double wStart = 0.1; wStart < 0.2; wStart += 0.1) {
+				Collections.sort(changeRates);
+				Collections.reverse(changeRates);
+				List<Double> weights = DataGenerator.generateLinear(wStart,
+						end, size);
+				//for (int i = 0; i < 50; i++) {
+					Collections.reverse(weights);
+					Win w = testOrder(changeRates, weights);
+					if (wins.get(w) == null) {
+						wins.put(w, 1);
+					} else {
+						wins.put(w, wins.get(w) + 1);
+					}
+					//System.out.print("   " + w + "   ");
+				//}
+			}
+		}
+		System.out.println("\n"+wins);
+	}
+
 	private static void testDbExample() {
 		List<Double> changes = new ArrayList<Double>();
 		List<Double> weights = new ArrayList<Double>();
@@ -183,7 +246,7 @@ public class TestCases {
 					+ (o2.weight * Math.exp(o2.changeRate)));
 		}
 	}
-	
+
 	private static class WeightedChangeRate implements Comparable {
 		double changeRate;
 
@@ -228,20 +291,19 @@ public class TestCases {
 	}
 
 	public static void main(String[] args) {
-		testExcel();
+		testSize();
 	}
 
-
-	private static List<Double> getInterestingWeights(){
+	private static List<Double> getInterestingWeights() {
 		List<Double> list = new ArrayList<Double>();
-		list.add(0.7);
-		list.add(0.9);
 		list.add(0.6);
 		list.add(0.8);
+		list.add(0.9);
+		list.add(0.7);
 		return list;
 	}
-	
-	private static List<Double> getMoreInterestingWeights(){
+
+	private static List<Double> getMoreInterestingWeights() {
 		List<Double> list = new ArrayList<Double>();
 		list.add(0.6);
 		list.add(0.7);
